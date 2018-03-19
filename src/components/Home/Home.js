6e53305431263bd515113ea8385b28b7c9297c58
andriytereschuk <shatskyat@gmail.com>
2018-03-19 15:10:12 +0200

    Notifications | formatting

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,9 +18,7 @@ function Home(props) {
       <h3>{props.dumbService.getHelloPhrase()}</h3>
       <h3>From config: {appConfig.baseUrl}</h3>
       <CounterContainer />
-      <button
-        onClick={props.notificationService.showNotify}
-      >
+      <button onClick={props.notificationService.showNotify}>
         show notification
       </button>
     </div>
--- src/utils/notificationService.js
+++ src/utils/notificationService.js
@@ -1,20 +1,21 @@
 import Notifications from 'react-notification-system-redux';
 import Store from '../configureStore';
 
-const notification = {
-  title: 'Hey, it\'s good to see you!',
-  message: 'Now you can see how easy it is to use notifications in React!',
-  position: 'tr',
-  autoDismiss: 0,
-  action: {
-    label: 'Click me!!',
-    callback: () => alert('clicked!')
-  }
-};
-
 class NotificationService {
   showNotify() {
-    Store.dispatch(Notifications.error(notification));
+    // mocked param
+    const param = {
+      title: 'Hey, it\'s good to see you!',
+      message: 'Now you can see how easy it is to use notifications in React!',
+      position: 'tl',
+      autoDismiss: 2,
+      action: {
+        label: 'Click me!!',
+        callback: () => alert('clicked!')
+      }
+    };
+
+    Store.dispatch(Notifications.error(param));
   }
 }
 
